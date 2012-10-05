ac5ab009e913b009f407055d2d46aadfc43a6c77
Gael Pasgrimaud <gael@gawel.org>
2012-10-05 03:18:13 +0200

    prepare release

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ version = '0.1'
 
 setup(name='impress',
       version=version,
-      description="",
-      long_description="""\
-""",
+      description=(
+          "Generate slides from rest files using sphinx and impress.js"),
+      long_description="""
+      Check the docs: http://gawel.github.com/impress/
+
+      And the source code: http://github.com/gawel/impress/
+      """,
       classifiers=[],
       keywords='sphinx impress.js',
       author='Gael Pasgrimaud',
       author_email='gael@gawel.org',
-      url='',
+      url='http://gawel.github.com/impress/',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
